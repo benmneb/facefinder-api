21271986b3bdb68377d41aefbda869663273a42b
Benjamin McCormick <benjamin@Benjamins-MacBook-Pro.local>
2020-04-14 14:06:20 +0800

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-parallel-84972',
     user : 'benjamin',
     password : '',
     database : 'smart-brain'
