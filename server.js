8538b9fbc200d9e33ddbf9f99fe9f8d3897217d9
Benjamin McCormick <benjamin@Benjamins-MacBook-Pro.local>
2020-04-14 14:11:38 +0800

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-parallel-84972',
-    user : 'benjamin',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+  	ssl: true,
   }
 });
 
